270b00f7810c60a86a34f15b6996b712c7125a01
N-K-Ad <ninad.kamal@education.nsw.gov.au>
2025-04-24 16:09:07 +1000

    Added back button

--- Assaignment.py
+++ Assaignment.py
@@ -24,18 +24,25 @@ learn_window.withdraw()  #hide the learn window initially
 def learn_button_clicked():
     learn_window.deiconify()  #show the learn window
     main_window.withdraw()  #hide the main window
+def back_button_clicked():
+    learn_window.withdraw()  #hide the learn window
+    main_window.deiconify()  #show the main window
 
 #Labels
 Title_label=Label(main_window, text= 'Bones Quiz', font=('Impact', 40), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
 Title_label.pack()
+
 message_label= Label(main_window, text='', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
 message_label.place(anchor=CENTER, relx=0.5, rely=0.5)
+
 test_label= Label(learn_window, text='Test your knowledge of the human skeleton', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
 test_label.pack()
+
 #Buttons
 Learn_button=Button(main_window, text='Learn', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= learn_button_clicked)
 Learn_button.pack(pady=20)
-
+back_Learn_button= Button(learn_window, text='Back', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= back_button_clicked)
+back_Learn_button.pack(pady=20)
 #Variables
 
 mainloop()
