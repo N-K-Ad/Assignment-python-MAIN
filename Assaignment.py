af7df8e69d721b636c6fe8811ece6b129999de25
N-K-Ad <ninad.kamal@education.nsw.gov.au>
2025-04-24 16:06:18 +1000

    Edited to make smoother transition between windows

--- Assaignment.py
+++ Assaignment.py
@@ -2,25 +2,38 @@
 from tkinter import *
 
 #Overall GUI
-window=Tk()  #initiate an instance of a window
-window.title('Bones quiz')
-window.geometry('1920x1080')
+
+#Main window
+main_window=Tk()  #initiate an instance of a window
+main_window.title('Bones quiz  Main menu')
+main_window.geometry('1920x1080')
 icon = PhotoImage(file='Logo.png')
-window.iconphoto(True,icon)
-window.config(bg='gray')
+main_window.iconphoto(True,icon)
+main_window.config(bg='#5c5c5c')
+
+#Learn window
+learn_window=Toplevel()
+learn_window.title('Bones quiz - Learn')
+learn_window.geometry('1920x1080')
+icon = PhotoImage(file='Logo.png')
+learn_window.iconphoto(True,icon)
+learn_window.config(bg='#5c5c5c')
+learn_window.withdraw()  #hide the learn window initially
 
 #Functions
 def learn_button_clicked():
-    message_label.config(text='Learn button clicked')
+    learn_window.deiconify()  #show the learn window
+    main_window.withdraw()  #hide the main window
 
 #Labels
-Title_label=Label(window, text= 'Bones Quiz', font=('Impact', 40), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
+Title_label=Label(main_window, text= 'Bones Quiz', font=('Impact', 40), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
 Title_label.pack()
-message_label= Label(window, text='', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
+message_label= Label(main_window, text='', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
 message_label.place(anchor=CENTER, relx=0.5, rely=0.5)
-
+test_label= Label(learn_window, text='Test your knowledge of the human skeleton', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
+test_label.pack()
 #Buttons
-Learn_button=Button(window, text='Learn', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= learn_button_clicked)
+Learn_button=Button(main_window, text='Learn', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= learn_button_clicked)
 Learn_button.pack(pady=20)
 
 #Variables
