0ae29b0c9e32549d35afee91edccf5ffc75663c5
N-K-Ad <ninad.kamal@education.nsw.gov.au>
2025-04-24 20:59:41 +1000

    Added main quiz menu

--- Assaignment.py
+++ Assaignment.py
@@ -1,8 +1,6 @@
 #bones quiz
 from tkinter import *
-
 #Overall GUI
-
 #Main window
 main_window=Tk()  #initiate an instance of a window
 main_window.title('Bones quiz  Main menu')
@@ -20,33 +18,62 @@ learn_window.iconphoto(True,icon)
 learn_window.config(bg='#5c5c5c')
 learn_window.withdraw()  #hide the learn window initially
 
+#Quiz menu window
+Quiz_menu_window=Toplevel()
+Quiz_menu_window.title('Bones quiz - Quiz menu')
+Quiz_menu_window.geometry('1920x1080')
+icon = PhotoImage(file='Logo.png')
+Quiz_menu_window.iconphoto(True,icon)
+Quiz_menu_window.config(bg='#5c5c5c')
+Quiz_menu_window.withdraw()  #hide the quiz menu window initially
+
 #Functions
+#learn button
 def learn_button_clicked():
     learn_window.deiconify()  #show the learn window
     main_window.withdraw()  #hide the main window
+    Quiz_menu_window.withdraw()  #hide the quiz menu window
+
+#quit button
 def back_button_clicked():
+    learn_window.withdraw() 
+    main_window.deiconify()  
+
+#quiz menu button
+def quiz_menu_button_clicked():
+    Quiz_menu_window.deiconify()  #show the quiz menu window
     learn_window.withdraw()  #hide the learn window
-    main_window.deiconify()  #show the main window
+    main_window.withdraw()  #hide the main window
 
 #Labels
 Title_label=Label(main_window, text= 'Bones Quiz', font=('Impact', 40), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
 Title_label.pack()
 
-message_label= Label(main_window, text='', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
-message_label.place(anchor=CENTER, relx=0.5, rely=0.5)
-
-test_label= Label(learn_window, text='Test your knowledge of the human skeleton', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
-test_label.pack()
+'''message_label= Label(main_window, text='', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20)
+message_label.place(anchor=CENTER, relx=0.5, rely=0.5)'''
 
 #Buttons
+
+#Main menu window buttons
 Learn_button=Button(main_window, text='Learn', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= learn_button_clicked)
 Learn_button.pack(pady=20)
+
+quit_button= Button(main_window, text='Quit', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= quit)
+quit_button.pack(pady=20) #quit button
+
+Quiz_menu_button=Button(main_window, text='Quiz menu', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= quiz_menu_button_clicked)
+Quiz_menu_button.pack(pady=20)
+
+#learn window buttons
 back_Learn_button= Button(learn_window, text='Back', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= back_button_clicked)
 back_Learn_button.pack(pady=20)
-quit_button= Button(main_window, text='Quit', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= quit)
-quit_button.pack(pady=20)
+
 quit_button= Button(learn_window, text='Quit', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= quit)
-quit_button.pack(pady=20)
+quit_button.pack(pady=20) #quit button
+
+Quiz_menu_button=Button(learn_window, text='Quiz menu', font=('Comic sans', 20), bg='gray', fg='black', bd=10, relief=RAISED, padx=20, pady=20, command= quiz_menu_button_clicked)
+Quiz_menu_button.pack(pady=20)
+
 #Variables
 
 mainloop()
